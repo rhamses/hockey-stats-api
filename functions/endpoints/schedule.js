b9310609df732d4b6fbcf1892d66f7b9744b5cc0
Rhamses Soares <rhamses@justos.com>
2022-11-06 12:23:26 -0300

    add schedule endpoint

---
+++ functions/endpoints/schedule.js
@@ -0,0 +1,68 @@
+const functions = require("firebase-functions");
+const express = require("express");
+const app = express();
+const axios = require("axios");
+
+function getTeam(games) {
+  const schedule = [];
+  for (const game of games) {
+    const {
+      status: { detailedState },
+      teams: {
+        away: {
+          score: awayScore,
+          team: { name: awayTeam },
+        },
+        home: {
+          score: homeScore,
+          team: { name: homeTeam },
+        },
+      },
+    } = game;
+
+    schedule.push({
+      detailedState,
+      teams: {
+        away: awayTeam,
+        home: homeTeam,
+      },
+      score: {
+        away: awayScore,
+        home: homeScore,
+      },
+    });
+  }
+  return schedule;
+}
+
+function formatGames(data) {
+  const { dates } = data;
+  const schedule = [];
+  for (const dt of dates) {
+    const { games } = dt;
+    schedule.push(getTeam(games));
+  }
+  return schedule;
+}
+
+app.get("/", async (req, res) => {
+  try {
+    const { startDate, endDate } = req.query;
+    const params = { startDate, endDate };
+    if (!startDate && !endDate)
+      throw new Error("startDate and EndDate required");
+
+    const { data } = await axios.get(
+      "https://statsapi.web.nhl.com/api/v1/schedule",
+      { params }
+    );
+    const schedule = formatGames(data);
+    res.send(schedule);
+  } catch (error) {
+    console.log(error);
+    const { message } = error;
+    res.status(400).send({ message });
+  }
+});
+
+module.exports = functions.https.onRequest(app);
